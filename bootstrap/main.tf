789a08425e3e952af3b30c5d863f8fbcac0878a3
Tomoya Yamanaka <3799961+kumo203@users.noreply.github.com>
2026-08-09 16:37:44 +0000

    Revert "Re-enable prevent_deletion_if_contains_resources = false on bootstrap RG"
    This reverts commit 5a698e1613d9dc9ab5bfc2526b09cfd4a0d4969f.

--- bootstrap/main.tf
+++ bootstrap/main.tf
@@ -15,16 +15,7 @@ terraform {
 }
 
 provider "azurerm" {
-  features {
-    resource_group {
-      # Azure auto-creates untracked resources in this RG as side effects of
-      # az_tf/'s Application Insights (a "Smart Detection" action group +
-      # alert rule) that Terraform never manages. Without this, destroying
-      # this resource group fails with "still contains Resources" even after
-      # every Terraform-managed resource inside it is gone.
-      prevent_deletion_if_contains_resources = false
-    }
-  }
+  features {}
 }
 
 data "azurerm_client_config" "current" {}
